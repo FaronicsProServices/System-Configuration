abef38fab9c99ec32e91fecddcdebd604dc36600
FaronicsProServices <proservices@faronics.com>
2024-11-21 01:21:34 +0530

    Update EnableSystemClockfromTaskbar.ps1

--- SystemIcons/EnableSystemClockfromTaskbar.ps1
+++ SystemIcons/EnableSystemClockfromTaskbar.ps1
@@ -1,2 +1,3 @@
 # Deletes the 'HideClock' registry key, restoring the system clock visibility in the taskbar.
+# Restart computer to apply changes.
 REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /V HideClock /F
