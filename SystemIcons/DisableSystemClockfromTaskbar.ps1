902fb5ed18162727fbdfe42edd97bc1b49eaf553
FaronicsProServices <proservices@faronics.com>
2024-11-21 01:21:55 +0530

    Update DisableSystemClockfromTaskbar.ps1

--- SystemIcons/DisableSystemClockfromTaskbar.ps1
+++ SystemIcons/DisableSystemClockfromTaskbar.ps1
@@ -1,2 +1,3 @@
 # Adds a registry key to hide the system clock in the taskbar by setting the 'HideClock' value to 1.
+# Restart computer to apply changes.
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /V HideClock /T REG_DWORD /D 1 /F
