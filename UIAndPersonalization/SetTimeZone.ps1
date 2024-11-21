90874788de3f020bea7c5d915a8b742bb71a4bb3
FaronicsProServices <proservices@faronics.com>
2024-11-21 20:43:22 +0530

    Create SetTimeZone.ps1

---
+++ UIAndPersonalization/SetTimeZone.ps1
@@ -0,0 +1,7 @@
+# Set the system's time zone to UTC. Replace "UTC" with the desired time zone ID.
+# Example: To set the system's time zone to Eastern Standard Time (EST), use "Eastern Standard Time"
+
+Set-TimeZone -Id "UTC"  # Sets the time zone to UTC
+
+# Example with a real time zone:
+# Set-TimeZone -Id "Eastern Standard Time"  # Sets the time zone to Eastern Standard Time (EST)
